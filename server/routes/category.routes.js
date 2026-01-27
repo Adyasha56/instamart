ce770da8cc7df5b2960065338c17b11e43f10eff
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-01-27 15:31:26 +0530

    feat: add admin authorization to create routes

--- server/routes/category.routes.js
+++ server/routes/category.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-
+import {authMiddleware,authorize} from "../middlewares/authMiddleware.js";
 import {createCategory,getCategories} from '../controllers/category.controller.js';
 
 const router = express.Router();
 
-router.post("/", createCategory);
+router.post("/",authMiddleware,authorize("admin"), createCategory);
 router.get("/",getCategories);
 
 export default router;
--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {createProduct,getProducts,getProductsByCategory} from "../controllers/product.controller.js";
+import { authMiddleware, authorize } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createProduct);
+router.post("/", authMiddleware,authorize("admin"), createProduct);
 router.get("/", getProducts);
 router.get("/category/:categoryId", getProductsByCategory);
 
