58d925199fef486cae4461e5f85bb0ba82be2f50
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-01-29 15:35:59 +0530

    feat: add email and phone types to user model

--- server/models/User.js
+++ server/models/User.js
@@ -11,9 +11,18 @@ const userSchema = new mongoose.Schema(
 
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
       lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+      unique: true,
+      trim: true,
       index: true,
     },
 
@@ -57,15 +66,9 @@ userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-
 // Compare passwords
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-
-
-const User = mongoose.model("User",userSchema);
-export default User;
-
-
+export const User = mongoose.model("User",userSchema);
