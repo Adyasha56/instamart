d3004ac39f7a2fc87c583afca9c7b093ef7e3a12
D3BAS1SH <Morning.Star00.111@protonmail.com>
2026-02-03 12:17:22 +0530

    feat: add category route for deleting a category

--- server/routes/category.routes.js
+++ server/routes/category.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {authMiddleware,authorize} from "../middlewares/authMiddleware.js";
-import {createCategory,getCategories} from '../controllers/category.controller.js';
+import {createCategory,getCategories,deleteCategories} from '../controllers/category.controller.js';
 
 const router = express.Router();
 
 router.post("/",authMiddleware,authorize("admin"), createCategory);
 router.get("/",getCategories);
+router.delete("/:id",authMiddleware,authorize("admin"),deleteCategories);
 
 export default router;
